393961243d8aa4f9d022a1149d32694e46ac0ec7
Shobhan S <shobhan.11.2000@gmail.com>
2022-03-15 23:12:36 +0530

    Swapping is done.

---
+++ src/Swap.java
@@ -0,0 +1,10 @@
+public class Swap {
+        public static void main(String[] args) {
+            Integer a = 2,b = 3,c;
+            System.out.println("The original numnbers:"+"\na:"+a+"\nb:"+b);
+            c = a;
+            a = b;
+            b = c;
+            System.out.println("After swapping:"+"\na:"+a+"\nb:"+b);
+        }
+    }
